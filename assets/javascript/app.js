762c36c6109cfd8f53ac98ff3088547fcdc9812b
Sharona Young <youngs_e@hotmail.com>
2017-12-13 09:45:25 -0500

    started building the counter

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,17 @@
+// timer to count down seconds left to answer questions, starts at 60
+var number = 60;
+var intervalId; 
+
+// function to run the counter, decreases by 1 every second
+function run() {
+  intervalId = setInterval(decrement, 1000);
+}
+
+// function to decrement number variable by 1
+function decrement() {
+number--;
+}
+
 // global variables
 // trivia game questions
 // game answers
@@ -6,6 +20,7 @@
 // function to start the game
 // on-click action for start button
 // function to end the game
-// timer to count down seconds left to answer questions
+
 // timer starts when start button clicked
 // show results, display correct answers, incorrect answers, and number of unanswered questions
+// code for check boxes
