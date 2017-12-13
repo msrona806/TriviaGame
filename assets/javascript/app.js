240b14504ddc59509d51d4b382146349175ef248
Sharona Young <youngs_e@hotmail.com>
2017-12-13 14:22:17 -0500

    trying to get the counter to stop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,8 @@
+$(document).ready (function() {
+// global variables
+
 // timer to count down seconds left to answer questions, starts at 60
-var number = 60;
+var number = 5;
 var intervalId; 
 
 // function to run the counter, decreases by 1 every second
@@ -12,18 +15,33 @@ function decrement() {
 number--;
 
 // display the counter on the game page
-$("#gameClock").html("<h1>" + number + "<h21");
+$("#gameClock").html("<h1>" + number + "<h1>");
 }
-// timer starts when start button clicked
-$("#start").on("click", run);
+// when start button clicked:,
 
-// global variables
+$("#start").click(run);
+$(".btn").click(remove);
+
+// stop the counter when it reaches 0
+if (number === 0) {
+  
+  //run stop function and display an alert
+  stop();
+  alert("Time's up!!!");
+}
+
+// function to stop the counter **NOT WORKING**
+function stop() {
+  clearInterval();
+}
+});
 // trivia game questions
+
 // game answers
 // for loop for questions
 // logic to compare answers to questions
 // function to start the game
-// on-click action for start button
+
 // function to end the game
 
 // show results, display correct answers, incorrect answers, and number of unanswered questions
