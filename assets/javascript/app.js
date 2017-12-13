1cb99c7c349bde96f09da1405bc448053060d05a
Sharona Young <youngs_e@hotmail.com>
2017-12-12 22:15:28 -0500

    added some notes on what needs to be completed

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// global variables
+// trivia game questions
+// game answers
+// for loop for questions
+// logic to compare answers to questions
+// function to start the game
+// on-click action for start button
+// function to end the game
+// timer to count down seconds left to answer questions
+// show results, display correct answers, incorrect answers, and number of unanswered questions
